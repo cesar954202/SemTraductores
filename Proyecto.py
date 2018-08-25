3fdd8dc45b3a0a7940fd50e58b60836688aa4da2
Unknown <cesar95.4202@gmail.com>
2018-08-25 11:19:20 -0500

    Pruebas python con tablas
    Aprendiendo tablas con python

--- Proyecto.py
+++ Proyecto.py
@@ -1,8 +1,26 @@
 print "Bienvenido a mi traductor".center(70,"=")
 
-#Se abre el archivo y se guarda en variable
+estados = [[00,01,02],
+           [10,11,13],
+           [20,21,22]
+           ]
 
-entrada = raw_input("Escribe el nombre de archivo: ")
-archivo = open(entrada, "r")
-contenido = archivo.read()
+salida = 1
+while salida !=0:
+    
+    #Se abre el archivo y se guarda en variable
+    entrada = raw_input("Escribe el nombre de archivo: ")
+    archivo = open(entrada, "r")
+    contenido = archivo.read()
+    print contenido
+
+
+    pos = int(input("Imprimir pos: "))
+    pos2 = int(input("Imprimir pos2: "))
+    aux = estados[pos][pos2]
+    print aux
+    
+    salida = int(input("Desea salir presione 0: "))
+
+    
 
